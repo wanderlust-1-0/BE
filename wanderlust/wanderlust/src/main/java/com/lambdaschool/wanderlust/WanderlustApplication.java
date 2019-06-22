547ace09c8e5c2f7ba280cbd058b8d5d3cc2aa22
Roger Caballero <rogercp1234@yahoo.com>
2019-06-22 13:09:43 -0700

    initial commit

---
+++ wanderlust/wanderlust/src/main/java/com/lambdaschool/wanderlust/WanderlustApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.wanderlust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderlustApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WanderlustApplication.class, args);
+    }
+
+}
